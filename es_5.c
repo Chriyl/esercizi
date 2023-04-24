7c9474a6351e7c9c26816e81ee0c213b6968974b
christian <christianmontanaro390@gmail.com>
2023-04-24 23:37:43 +0200

    scheletro del es 5 di informatica

---
+++ es_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 100
+
+/*
+ Realizzare un array di record contenente un elenco degli studenti di un corso di studi del tuo istituto
+ riportante il numero di matrico,
+ il cognome, il nome e il numero di giorni di assenza.
+ Scrivere un programma che permetta di ordinare tale elenco su un campo richiesto in input.
+*/
+
+typedef struct {
+    char nome[20];
+    char cognome[20];
+    char matricola[20];
+    int assenze;
+
+} studenti;
+
+int main() {
+    studenti studente[N];
+    int check = 0;
+
+    do {
+
+    }while(chcek !=6);
+
+    return 0;
+}
